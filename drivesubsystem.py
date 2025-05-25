3fc0d1cd77580b2d38939534d37053fe4b03a834
yen200897 <chiyenlucas@gmail.com>
2025-05-26 00:16:00 +0800

    Add files via upload

---
+++ drivesubsystem.py
@@ -0,0 +1,36 @@
+import rev
+import commands2
+import wpilib
+from wpilib.drive import DifferentialDrive
+from rev import SparkMax
+from rev import SparkMaxConfig
+import wpilib.drive
+
+class Drivesubsystem(commands2.Subsystem):
+    def __init__(self):    
+        self.left1 = SparkMax(1, SparkMax.MotorType.kBrushed)
+        self.left2 = SparkMax(2, SparkMax.MotorType.kBrushed)
+        self.right1 = SparkMax(3, SparkMax.MotorType.kBrushed)
+        self.right2 = SparkMax(4, SparkMax.MotorType.kBrushed)
+
+
+        cfg_left1 = SparkMaxConfig()
+        self.left1.configure(cfg_left1, SparkMax.ResetMode.kNoResetSafeParameters, SparkMax.PersistMode.kNoPersistParameters)
+
+        cfg_left2 = SparkMaxConfig()
+        cfg_left2.follow(self.left1, False)
+        self.left2.configure(cfg_left2, SparkMax.ResetMode.kNoResetSafeParameters, SparkMax.PersistMode.kNoPersistParameters)
+
+        cfg_right1 = SparkMaxConfig()
+        cfg_right1.inverted(True)
+        self.right1.configure(cfg_right1, SparkMax.ResetMode.kNoResetSafeParameters, SparkMax.PersistMode.kNoPersistParameters)
+
+        cfg_right2 = SparkMaxConfig()
+        cfg_right2.follow(self.right1, False)
+        self.right2.configure(cfg_right2, SparkMax.ResetMode.kNoResetSafeParameters, SparkMax.PersistMode.kNoPersistParameters)
+
+        # 建立差速驅動物件
+        self.drive = DifferentialDrive(self.left1, self.right1)
+        self.drive.setMaxOutput(0.5)
+
+    def
---
+++ robot.py
@@ -0,0 +1,10 @@
+# TODO: insert robot code here
+import commands2
+from robotcontainer import RobotContainer  # 匯入 RobotContainer 類別
+
+# 定義機器人主類別，繼承自 commands2.TimedCommandRobot
+class MyRobot(commands2.TimedCommandRobot):
+    # 機器人初始化方法
+    def robotInit(self):
+        # 創建 RobotContainer 的實例，管理子系統、指令和控制器
+        self.container = RobotContainer() 
